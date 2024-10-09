af45bc4b9d6b99e1c7d84146c580fbef6330d9b7
ハムザ ラフィーク <38761754+RafikHamza@users.noreply.github.com>
2024-10-09 14:34:15 +0900

    Initial commit

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,44 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import livereload from 'rollup-plugin-livereload';
 import postcss from 'rollup-plugin-postcss';
-import preprocess from 'svelte-preprocess'; // Import preprocess
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/main.js',
-  output: {
-    sourcemap: true,
-    format: 'iife', // Change as needed (e.g., 'es', 'cjs')
-    name: 'app',
-    file: 'public/build/bundle.js'
-  },
-  plugins: [
-    svelte({
-      preprocess: preprocess(), // Add preprocess here
-      // Enable runtime checks when not in production
-      compilerOptions: {
-        dev: !production
-      },
-      emitCss: true // Extract CSS if true
-    }),
-    resolve({
-      browser: true,
-      dedupe: ['svelte']
-    }),
-    commonjs(),
-    postcss({ extract: true }), // Ensure postcss is included
-    production && terser(),
-    livereload('public'), // Enable livereload
-  ],
-  watch: {
-    clearScreen: false
-  }
+    input: 'src/main.js',
+    output: {
+        sourcemap: true,
+        format: 'iife', // Immediately Invoked Function Expression — for browsers
+        name: 'app',
+        file: 'public/build/bundle.js'
+    },
+    plugins: [
+        svelte({
+            // Use compilerOptions instead of dev and css
+            compilerOptions: {
+                dev: !production,
+            }
+        }),
+        // Add postcss to the plugins array
+        postcss({
+            extract: true, // Extract CSS into separate file
+            minimize: production,
+        }),
+        // Resolve modules
+        resolve({
+            browser: true,
+            dedupe: ['svelte']
+        }),
+        commonjs(),
+        // Minify the bundle in production mode
+        production && terser(),
+        // Live reload for development
+        livereload('public')
+    ],
+    watch: {
+        clearScreen: false
+    }
 };
